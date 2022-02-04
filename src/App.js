bc60aa0dfff37f9621ee8e12405f767a221e945a
Dewaley <adewaleademilua@gmail.com>
2022-02-04 13:18:17 +0100

    DESKTOP DESIGN AND TITLE MODIFICATION

--- src/App.js
+++ src/App.js
@@ -1,116 +1,116 @@
-import "./App.css";
-import { useState } from "react";
+import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState('')
 
   const handleClick = (e) => {
-    if (result != "ERROR") {
-      setResult(result.concat(e.target.name));
-    }else {
+    if (result !== 'ERROR') {
+      setResult(result.concat(e.target.name))
+    } else {
       setResult(e.target.name)
     }
-  };
+  }
   const clearAll = (e) => {
-    setResult("");
-  };
+    setResult('')
+  }
   const clear = (e) => {
-    setResult(result.slice(0, -1));
-  };
+    setResult(result.slice(0, -1))
+  }
   const solve = (e) => {
     try {
-      setResult(eval(result).toString());
+      setResult(eval(result).toString())
     } catch (err) {
-      setResult("ERROR");
+      setResult('ERROR')
     }
-  };
+  }
   const toggleTheme2 = (e) => {
-    const bottom = document.getElementsByClassName("bottom")
-    bottom.style.justifyContent = "center"
+    const bottom = document.getElementsByClassName('bottom')
+    bottom.style.justifyContent = 'center'
   }
   return (
-    <div className="container">
-      <div className="main-container">
+    <div className='container'>
+      <div className='main-container'>
         <header>
           <h1>calc</h1>
-          <div className="right">
+          <div className='right'>
             <h3>THEME</h3>
-            <div className="theme">
-              <div className="top">
-                <span>1</span>
-                <span>2</span>
-                <span>3</span>
+            <div className='theme'>
+              <div className='top'>
+                <span className='theme1'>1</span>
+                <span className='theme2'>2</span>
+                <span className='theme3'>3</span>
               </div>
-              <div className="bottom">
-                <span className="check"></span>
+              <div className='bottom'>
+                <span className='check'></span>
               </div>
             </div>
           </div>
         </header>
-        <div className="calc">
+        <div className='calc'>
           <form>
-            <input type="text" value={result} />
+            <input type='text' value={result} />
           </form>
-          <div className="keypad">
-            <button name="7" onClick={handleClick}>
+          <div className='keypad'>
+            <button name='7' onClick={handleClick}>
               7
             </button>
-            <button name="8" onClick={handleClick}>
+            <button name='8' onClick={handleClick}>
               8
             </button>
-            <button name="9" onClick={handleClick}>
+            <button name='9' onClick={handleClick}>
               9
             </button>
-            <button className="rem" onClick={clear}>
+            <button className='rem' onClick={clear}>
               DEL
             </button>
-            <button name="4" onClick={handleClick}>
+            <button name='4' onClick={handleClick}>
               4
             </button>
-            <button name="5" onClick={handleClick}>
+            <button name='5' onClick={handleClick}>
               5
             </button>
-            <button name="6" onClick={handleClick}>
+            <button name='6' onClick={handleClick}>
               6
             </button>
-            <button name="+" onClick={handleClick}>
+            <button name='+' onClick={handleClick}>
               +
             </button>
-            <button name="1" onClick={handleClick}>
+            <button name='1' onClick={handleClick}>
               1
             </button>
-            <button name="2" onClick={handleClick}>
+            <button name='2' onClick={handleClick}>
               2
             </button>
-            <button name="3" onClick={handleClick}>
+            <button name='3' onClick={handleClick}>
               3
             </button>
-            <button name="-" onClick={handleClick}>
+            <button name='-' onClick={handleClick}>
               -
             </button>
-            <button name="." onClick={handleClick}>
+            <button name='.' onClick={handleClick}>
               .
             </button>
-            <button name="0" onClick={handleClick}>
+            <button name='0' onClick={handleClick}>
               0
             </button>
-            <button name="/" onClick={handleClick}>
+            <button name='/' onClick={handleClick}>
               /
             </button>
-            <button name="*" onClick={handleClick}>
+            <button name='*' onClick={handleClick}>
               x
             </button>
-            <button className="rem reset" onClick={clearAll}>
+            <button className='rem reset' onClick={clearAll}>
               RESET
             </button>
-            <button className="equate" onClick={solve}>
+            <button className='equate' onClick={solve}>
               =
             </button>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
